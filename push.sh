0896e46dc2604d6fd2622de261bae1dc5c98d402
JDagsa <jayden.dagsa@gmail.com>
2020-12-26 21:19:58 +0800

    Added favicon

---
+++ push.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Beginning push."
+git add .
+echo "Added all files."
+echo "Enter your commit message:"
+read COMMIT
+git commit -m "$COMMIT"
+echo "Committed files. If it was not, then you can check the message above for what to do."
+git push origin master
+echo "Pushed commits to GitHub! If it didn't succeed, check the above message for what to do."
+
