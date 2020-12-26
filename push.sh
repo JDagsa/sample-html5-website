52de72a373b4c461f7a0688e0213bd30787501a9
JDagsa <jayden.dagsa@gmail.com>
2020-12-26 21:23:47 +0800

    Fixed favicon. Should work now.

--- push.sh
+++ push.sh
@@ -6,7 +6,8 @@ echo "Added all files."
 echo "Enter your commit message:"
 read COMMIT
 git commit -m "$COMMIT"
-echo "Committed files. If it was not, then you can check the message above for what to do."
+echo "Committed files. If it didn't succeed, check the message above for what to do."
+echo "Attempting push to GitHub..."
 git push origin master
 echo "Pushed commits to GitHub! If it didn't succeed, check the above message for what to do."
 
